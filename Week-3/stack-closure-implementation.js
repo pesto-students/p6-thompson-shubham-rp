1c510fecb504e460db2eef986b27136b741f21be
Shubham Phadte <shubhamphadte37@gmail.com>
2022-10-29 09:01:21 +0530

    Week 3 - Changed method name --> getItems() to getStack() in Stack implementation using closures

--- Week-3/stack-closure-implementation.js
+++ Week-3/stack-closure-implementation.js
@@ -2,7 +2,7 @@ function createStack() {
   let items = [];
 
   return {
-    getItems() {
+    getStack() {
       return items;
     },
     push(item) {
@@ -16,19 +16,19 @@ function createStack() {
 }
 
 const stack = createStack();
-console.log(stack.getItems()); // will fetch the stack --> output --> []
+console.log(stack.getStack()); // will fetch the stack --> output --> []
 stack.push(10);
 console.log(stack.items); // output will be "undefined" because we are not getting direct access to the stack(i.e. items)
 stack.push(5);
 
-console.log(stack.getItems()); //after the above 2 push operations output will be [10,5]
+console.log(stack.getStack()); //after the above 2 push operations output will be [10,5]
 stack.pop();
 
 stack.items; // still can't access
 stack.items = [10, 100, 1000]; // this shouldn't happen - this line is basically changing the stack from outside. Here we are not changing the original stack.
 // We are creating a new stack.items array with it's own scope & environment. it should be possible to change the stack only via push() & pop() methods
 console.log(stack.items); // output --> [10,100,1000]. not the original stack
-console.log(stack.getItems()); // after pop() operation --> output [10]
+console.log(stack.getStack()); // after pop() operation --> output [10]
 stack.push(5);
 console.log(stack.items); // still [10,100,1000]
-console.log(stack.getItems()); // after above push(5) operation, output--> [10,5]
+console.log(stack.getStack()); // after above push(5) operation, output--> [10,5]
